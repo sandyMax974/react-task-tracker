0b7b1fb0f136f6ac954846c99ae2f1928721a700
sandy <pounoussamy.sandy@gmail.com>
2021-04-29 16:20:26 +0100

    Reinstall npm packages to correct error due to babel-loader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Header from './components/Header'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div className="container">
       <Header /> 
     </div>
   );
